aba93f442141bbcb9d6f939af4e4ce87f7052aab
Pedro Lisboa <165230409+pedrolisboac@users.noreply.github.com>
2025-07-10 03:58:02 +0000

    desafio entregue

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuário e concatená-los 
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenasdas são: ", info_concatenada)
---
+++ resolucoes_code/repet_txt.py
@@ -0,0 +1,7 @@
+# Agora vamos solicitar uma string e um número inteiro como entrada. Depois iremos retornar a string de forma repetida de acordo com o número informado.
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((string + ' ') * numero)
+
